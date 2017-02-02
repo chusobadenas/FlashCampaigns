a409b48861118fd465f478a304d46bdb72166858
Jesús Badenas <chusobadenas@gmail.com>
2017-02-02 16:23:11 +0100

    Mapper between entity and response

---
+++ app/src/main/java/com/flashcampaigns/app/data/entity/mapper/CampaignMapper.java
@@ -0,0 +1,50 @@
+package com.flashcampaigns.app.data.entity.mapper;
+
+import com.flashcampaigns.app.data.entity.Campaign;
+import com.flashcampaigns.app.data.entity.response.CampaignResponse;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Mapper class used to transform a {@link CampaignResponse} into a {@link Campaign}.
+ */
+@Singleton
+public class CampaignMapper {
+
+  @Inject
+  public CampaignMapper() {
+    // Empty constructor
+  }
+
+  /**
+   * Transform a {@link CampaignResponse} into a {@link Campaign}.
+   *
+   * @param campaignResponse object to be transformed.
+   * @return {@link Campaign} if valid {@link CampaignResponse}, otherwise null.
+   * @throws ParseException when a date is not valid
+   */
+  public Campaign transform(CampaignResponse campaignResponse) throws ParseException {
+    Campaign campaign = null;
+
+    if (campaignResponse != null) {
+      int id = campaignResponse.id();
+      String name = campaignResponse.name();
+      String imageUrl = campaignResponse.imageUrl();
+
+      DateFormat formatter = new SimpleDateFormat("yyyy-mm-dd", Locale.getDefault());
+      Date startDate = formatter.parse(campaignResponse.startDate());
+      Date endDate = formatter.parse(campaignResponse.endDate());
+
+      campaign = Campaign.create(id, name, startDate, endDate, imageUrl);
+    }
+
+    return campaign;
+  }
+}
---
+++ app/src/test/java/com/flashcampaigns/app/data/entity/mapper/CampaignMapperTest.java
@@ -0,0 +1,39 @@
+package com.flashcampaigns.app.data.entity.mapper;
+
+import com.flashcampaigns.app.data.entity.Campaign;
+import com.flashcampaigns.app.data.entity.response.CampaignResponse;
+
+import org.junit.Test;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+public class CampaignMapperTest {
+
+  @Test
+  public void testTransformCampaignSuccess() {
+    DateFormat formatter = new SimpleDateFormat("yyyy-mm-dd", Locale.getDefault());
+    CampaignMapper mapper = new CampaignMapper();
+
+    try {
+      CampaignResponse campaignResponse = CampaignResponse.create(1, "My campaign", "01-01-2017", "01-01-2018", "url");
+      Campaign campaign = mapper.transform(campaignResponse);
+
+      assertNotNull(campaign);
+      assertEquals(campaign.id(), 1);
+      assertEquals(campaign.name(), "My campaign");
+      assertEquals(campaign.startDate().getTime(), formatter.parse("01-01-2017").getTime());
+      assertEquals(campaign.endDate().getTime(), formatter.parse("01-01-2018").getTime());
+      assertEquals(campaign.imageUrl(), "url");
+
+    } catch (ParseException exception) {
+      fail(exception.getMessage());
+    }
+  }
+}

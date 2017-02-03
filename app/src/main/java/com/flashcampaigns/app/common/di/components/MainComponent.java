802e6ddc72a224c5bccf3fcf305d3f933f1f0eda
Jesús Badenas <chusobadenas@gmail.com>
2017-02-03 15:51:12 +0100

    Injecting fragment

--- app/src/main/java/com/flashcampaigns/app/common/di/components/MainComponent.java
+++ app/src/main/java/com/flashcampaigns/app/common/di/components/MainComponent.java
@@ -3,7 +3,9 @@ package com.flashcampaigns.app.common.di.components;
 import com.flashcampaigns.app.common.di.PerActivity;
 import com.flashcampaigns.app.common.di.modules.ActivityModule;
 import com.flashcampaigns.app.common.di.modules.CampaignModule;
+import com.flashcampaigns.app.common.di.modules.ProductModule;
 import com.flashcampaigns.app.presentation.main.MainFragment;
+import com.flashcampaigns.app.presentation.product.ProductFragment;
 
 import dagger.Component;
 
@@ -11,8 +13,11 @@ import dagger.Component;
  * A scope {@link PerActivity} component.
  */
 @PerActivity
-@Component(dependencies = ApplicationComponent.class, modules = {ActivityModule.class, CampaignModule.class})
+@Component(dependencies = ApplicationComponent.class, modules = {ActivityModule.class, CampaignModule.class, ProductModule
+    .class})
 public interface MainComponent extends ActivityComponent {
 
   void inject(MainFragment mainFragment);
+
+  void inject(ProductFragment productFragment);
 }
